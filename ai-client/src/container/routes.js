3bbbcce1662501a719fe3b1a8e24610653b1051e
b117014 <b117014@iiit-bh.ac.in>
2020-06-07 18:51:35 +0530

    initial routes

---
+++ ai-client/src/container/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const RouterContent = (props)=>{
+
+
+    return(
+        <Router>
+            <Switch>
+                <Route exact path="/" component={} />
+                <Route exact path="/register" component={} />
+            </Switch>
+        </Router>
+    )
+}
+
+export {RouterContent}
