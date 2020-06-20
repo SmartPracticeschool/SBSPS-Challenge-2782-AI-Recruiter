9b1bf76b350dd4263d677841bbb8595223c37051
b117020 <b117020@iiit-bh.ac.in>
2020-06-20 21:44:07 +0530

    Added ML model for Resume Parsing

---
+++ ML MODELS/ExtractInfo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 20 19:58:32 2020
+
+@author: Devdarshan
+"""
+
+from pyresparser import ResumeParser
+data = ResumeParser('Sample Resume\CV.pdf').get_extracted_data()
+
