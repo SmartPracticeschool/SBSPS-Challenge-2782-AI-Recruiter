66fa87ff0fa7c47b823c880b8a4f967c29928020
b117020 <b117020@iiit-bh.ac.in>
2020-06-20 23:24:52 +0530

    updated files

--- ML MODELS/ExtractInfo.py
+++ ML MODELS/ExtractInfo.py
@@ -5,6 +5,6 @@ Created on Sat Jun 20 19:58:32 2020
 @author: Devdarshan
 """
 
-from pyresparser import ResumeParser
-data = ResumeParser('Sample Resume\CV.pdf').get_extracted_data()
+from Resume_parser import ResumeParser
+data = ResumeParser('Sample Resume\cv2.pdf').get_extracted_data()
 
