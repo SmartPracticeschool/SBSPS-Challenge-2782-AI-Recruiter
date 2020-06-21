510c80c6a819385295b3c6b2e86513df98958bb7
b117014 <b117014@iiit-bh.ac.in>
2020-06-21 17:58:08 +0530

    use resume parser to get all info of user

--- ai-server/routes/user.js
+++ ai-server/routes/user.js
@@ -2,7 +2,9 @@ const express =  require('express');
 const router  = express.Router();
 const db = require('../models')
 const multer = require('multer');
+const ResumeParser = require('resume-parser')
 const {UserLogin, UserRegister} = require('../handlers/auth')
+const {spawn} = require('child_process')
 
 
 const storege =  multer.diskStorage({
@@ -57,5 +59,21 @@ router.post('/user/:id/resume', upload.single('file') ,async (req,res,next)=>{
                         })
                     }
 } )
+
+router.get('/resume-parser', (req,res,next)=>{
+        // ResumeParser
+        //     .parseResumeUrl('http://localhost:5000/Resume-1592674231670CV.pdf')
+        //     .then(res=>console.log(res))
+        //     .catch(err=>console.log(err))
+        let processData = spawn('python', ['./ml/ExtractInfo.py', 'public/resume/Resume-1592674231670CV.pdf'])
+        processData.stdout.on('data', (data)=>{
+            console.log(data.toString())
+        })
+        processData.stderr.on('data', (err)=>{
+            console.log(err.toString())
+        })
+        console.log('by')
+
+})
 
 module.exports = router
