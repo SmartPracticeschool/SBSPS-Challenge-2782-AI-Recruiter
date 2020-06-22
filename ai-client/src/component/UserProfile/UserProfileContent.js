0fbfac166060df0d2f2977061b063b8dd3832db3
b117014 <b117014@iiit-bh.ac.in>
2020-06-23 03:32:55 +0530

    user profile ui

--- ai-client/src/component/UserProfile/UserProfileContent.js
+++ ai-client/src/component/UserProfile/UserProfileContent.js
@@ -27,28 +27,64 @@ class UserProfileContent extends React.Component{
                                       <div className='row'>
                                           <div className='col-md-6'>
                                               <i>Name</i>
-                                          </div>
-                                          <div className='col-md-6'>
                                               <h6>Prabhat Kumar</h6>
                                           </div>
-                                      </div>
-                                      <div className='row'>
                                           <div className='col-md-6'>
                                               <i>College</i>
-                                          </div>
-                                          <div className='col-md-6'>
                                               <h6>IIIT bhubaneswar</h6>
                                           </div>
                                       </div>
                                       <div className='row'>
                                           <div className='col-md-6'>
                                               <i>Contact</i>
-                                          </div>
-                                          <div className='col-md-6'>
                                               <h6>9668640141</h6>
                                           </div>
+                                          <div className='col-md-6'>
+                                              
+                                          </div>
                                       </div>
+                                      <div className='row'>
+                                          <i>skills</i>
+                                          <p></p>
+                                       
+                                      </div>
+                                      <div className='row'>
+                                          <i>experience</i>
+                                          <p></p>
+                                      </div>
+                                  </div>
+                                  <hr />
+                                  <div className="">
+                                        <form>
+                                            <div className="row">
+                                                <div className="col-lg-6 col-md-6">
+                                                    <div className="form-group">
+                                                        <label>Full Name</label>
+                                                        <input
+                                                        value={this.state.name}
+                                                        name="name"
+                                                        onChange={(e)=>this.setState({name:e.target.value})}
+                                                        placeholder="name"
+                                                        className="form-control"
+                                                        />
+                                                    </div>
 
+                                                </div>
+                                                <div className="col-lg-6 col-md-6">
+                                                    <div className="form-group">
+                                                        <label>Email</label>
+                                                        <input
+                                                        value={this.state.email}
+                                                        name="email"
+                                                        onChange={(e)=>this.setState({email:e.target.value})}
+                                                        placeholder="email"
+                                                        className="form-control"
+                                                        />
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </form>
                                   </div>
                               </div>
                           </div>
