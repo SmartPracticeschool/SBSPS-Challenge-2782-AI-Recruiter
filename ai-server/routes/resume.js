de7a826b6868f4b497ff312d02159ca99318d1d4
b117014 <b117014@iiit-bh.ac.in>
2020-06-22 17:25:34 +0530

    seperate routes for resume uploading

--- ai-server/routes/resume.js
+++ ai-server/routes/resume.js
@@ -1,3 +1,50 @@
 const express = require('express');
 const db = require('../models');
 const router = express.Router();
+const multer = require('multer');
+const { route } = require('./user');
+
+
+const storege =  multer.diskStorage({
+    destination: './public/resume',
+    filename: function(req, file, cb){
+         cb(null, 'Resume-' + Date.now() + file.originalname)
+    }
+})
+const upload = multer({storage: storege})
+
+
+
+router.post('/user/:id/resume', upload.single('file') ,async (req,res,next)=>{
+
+    try{
+        let url =''
+        if(req.hostname == 'localhost'){
+             url =  req.protocol + '://' + req.hostname + ':5000/' + req.file.filename;
+        }else{
+            url = req.protocol + '://' + req.hostname + '/' + req.file.filename
+        }
+       
+         let user = await db.User.findById(req.params.id)
+         if(user){
+             user.resume = url
+             await user.save();
+             return res.send(url)
+         }
+         else{
+             return next({
+                 status: 404,
+                 message: 'user does not exist'
+             })
+         }
+        
+    }
+    catch(err){
+        return next({
+            status: 400,
+            error: err.message || 'something went wrong'
+        })
+    }
+} )
+
+module.exports = router
