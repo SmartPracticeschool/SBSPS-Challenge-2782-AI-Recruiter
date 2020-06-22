d3b1441a0e7f10c9271b1adb5f67584a57341982
b117014 <b117014@iiit-bh.ac.in>
2020-06-23 03:22:56 +0530

    user profile ui

--- ai-client/src/component/HomePage/HomePage.js
+++ ai-client/src/component/HomePage/HomePage.js
@@ -6,7 +6,7 @@ const Homepage = (props)=>{
     return(
         <div>
             <section className="p-0 m-0 main-page" data-bg-img='./image/home-1.jpg'>
-                <div className=" p-5 w-50">
+                <div className=" p-5 w-50 ">
                     <h1>Ai Recruitment</h1>
                     <h5>Based On Our Ai recruitment Platform Company Can easily hire the employ</h5>
                 </div>
