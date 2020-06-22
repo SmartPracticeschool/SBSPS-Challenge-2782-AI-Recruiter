04815d72f7309c97fdf0d5f0c0bd27ecf844e643
b117014 <b117014@iiit-bh.ac.in>
2020-06-22 19:10:54 +0530

    working on ui of homepage

--- ai-client/src/component/HomePage/HomePage.js
+++ ai-client/src/component/HomePage/HomePage.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import './home.css'
 
 const Homepage = (props)=>{
 
     return(
         <div>
-            <h1>Home Page</h1>
+            <section className="p-0 m-0 main-page" data-bg-img='./image/home-1.jpg'>
+                <div className=" p-5 w-50">
+                    <h1>Ai Recruitment</h1>
+                    <h5>Based On Our Ai recruitment Platform Company Can easily hire the employ</h5>
+                </div>
+            </section>
         </div>
     )
 }
