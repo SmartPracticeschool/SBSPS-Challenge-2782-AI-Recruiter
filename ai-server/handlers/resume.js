ac7a79ca17f686757af1664bcc5aa91eea1a3538
b117014 <b117014@iiit-bh.ac.in>
2020-06-23 21:24:11 +0530

    handlers to upload video add add to the user database

--- ai-server/handlers/resume.js
+++ ai-server/handlers/resume.js
@@ -56,3 +56,32 @@ exports.ResumeUpload = async (req,res,next)=>{
         })
     }
 }
+
+exports.VideoUpload = async (req,res, next)=>{
+
+                try{
+                         let user = await db.User.findById(req.params.id)
+                         if(user){
+                             let url = ''
+                             if(req.hostname == 'localhost'){
+                                 url = req.protocol + '://' + req.hostname + ':5000/' + req.file.filename
+                             }else{
+                                 url = req.protocol + '://' + req.hostname + '/' + req.file.filename
+                             }
+                             user.video = url;
+                             await user.save()
+                             res.send(url)
+
+                         }else{
+                             return next({
+                                 status: 404,
+                                 message: 'user does not exist'
+                             })
+                         }
+                }catch(err){
+                    return next({
+                        status: 400,
+                        message: err.message
+                    })
+                }
+            }
