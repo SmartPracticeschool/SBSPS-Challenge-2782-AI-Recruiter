3fbfe8558b2988e75789e6757bcbdc963e5f8136
b117014 <b117014@iiit-bh.ac.in>
2020-07-02 03:12:06 +0530

    user login page

---
+++ ai-client/src/component/LoginPage/LoginPage.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { LoginPageContent } from './LoginPageContent';
+import { UserLogin } from '../_Api/User';
+import { setToken } from '../service/setToken';
+import { UserAuth } from '../../redux/actionCreater/user';
+
+class LoginPage extends React.Component{
+
+    onSubmitCallback = (value)=>{
+        console.log(value)
+        UserLogin(value)
+            .then(res=>{
+                setToken(res.data.token)
+                localStorage.setItem('jwtToken', res.data.token)
+            })
+    }
+    componentDidUpdate(){
+        if(!this.props.user.isAuthenticated){
+            this.props.history.push('/')
+        }
+       
+    }
+
+    render(){
+       
+        return(
+            <LoginPageContent 
+            onSubmitCallback={this.onSubmitCallback}
+            />
+        )
+    }
+}
+
+function mapStateToDispatch(dispatch){
+    return {
+        userAdd: (data)=>dispatch(UserAuth(data))
+    }
+}
+function mapStateToProps(state){
+    return{
+        user: state.user.user
+    }
+}
+
+LoginPage = connect(mapStateToProps, mapStateToDispatch)(LoginPage)
+export {LoginPage}
--- ai-client/src/container/NavBar/Navbar.js
+++ ai-client/src/container/NavBar/Navbar.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import { UserLogout } from '../../redux/actionCreater/user';
+import {connect} from 'react-redux'
 
+const NavBars = (props)=>{
 
-const NavBar = (props)=>{
+    const Logout = ()=>{
+            localStorage.clear()
+            props.logout()
+    }
+    console.log(props.user.isAuthenticated)
     return(
         <nav>
             <div className='content'>
-                <div>Home</div>
-                <div>Register</div>
-                <div>Login</div>
+                {props.user.isAuthenticated ? ( <div><a href="#" onClick={Logout} >Logout</a></div>)
                 
+            : (<>
+            <div>Home</div>
+                <div>Register</div>
+                <div>Login</div></>)}
+                
+               
             </div>
         </nav>
     )
 }
 
+function mapStateToProps(state){
+    return{
+        user: state.user.user
+    }
+}
+
+function mapStateToDispatch(dispatch){
+    return{
+        logout: ()=> dispatch(UserLogout)
+    }
+}
+const NavBar = connect(mapStateToProps, mapStateToDispatch)(NavBars)
 export {NavBar}
